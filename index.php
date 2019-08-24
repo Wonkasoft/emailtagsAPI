22278050e447b9be46428177e74ea44f83c5ee22
MrListe <rlister@wonkasoft.com>
2019-08-24 00:44:39 -0700

    updated api

--- index.php
+++ index.php
@@ -55,11 +55,20 @@ if ( empty( $_GET ) ) {
 			}
 		endif;
 
+		if ( $campaign_name ) :
+			$campaign_list = get_a_list_of_campaigns( $campaign_name );
+			foreach ( $campaign_list as $campaign ) :
+				if ( $campaign_name === $campaign->name ) :
+					$campaign_obj = $campaign;
+				endif;
+			endforeach;
+		endif;
+
 		if ( $campaign_name && $affiliate_code && $affiliate_link ) :
 			$prep_data = array(
 				'email'             => $email,
 				'tag_id'            => $tag_obj->tagId,
-				'campaign_name'     => $campaign_name,
+				'campaign_id'       => $campaign_obj->campaignId,
 				'affiliate_code'    => $affiliate_code,
 				'affiliate_link'    => $affiliate_link,
 			);
@@ -379,4 +388,60 @@ function get_contact_list( $email ) {
 
 		  return $response;
 	  endif;
+}
+
+/**
+ * This function will get a list of campaigns.
+ *
+ * @rest_endpoint GET /campaigns
+ * @param  string $campaign_name contains the name of the campaign passed in.
+ * @return object                returns object of campaigns.
+ */
+function get_a_list_of_campaigns( $campaign_name ) {
+	  $headerdata = array(
+		  'X-Auth-Token: api-key ' . $GLOBALS['token'],
+		  'Content-Type: application/x-www-form-urlencoded',
+	  );
+
+		$post_data  = array(
+			'query' => array(
+				'name'           => $campaign_name,
+				'isDefault'     => null,
+			),
+			'sort'             => array(
+				'name'          => null,
+				'createdOn'      => null,
+			),
+			'fields'           => null,
+			'perPage'          => null,
+			'page'             => null,
+		);
+
+		$post_data  = json_decode( json_encode( $post_data ) );
+		$post_data  = http_build_query( $post_data );
+
+		$ch         = curl_init();
+		$url        = $api_url . '/campaigns?' . $post_data;
+		curl_setopt( $ch, CURLOPT_URL, $url );
+		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+		curl_setopt( $ch, CURLOPT_HEADER, false );
+		curl_setopt( $ch, CURLOPT_HTTPHEADER, $headerdata );
+		curl_setopt( $ch, CURLPROTO_HTTPS, true );
+
+		$response   = curl_exec( $ch );
+
+		if ( false === $response ) :
+			curl_close( $ch );
+			$error_obj = array(
+				'error' => curl_error( $ch ),
+				'status'    => 'failed',
+			);
+			$error_obj = json_decode( $error_obj );
+			return $error_obj;
+		else :
+			curl_close( $ch );
+			$response = json_decode( $response );
+
+			return $response;
+		endif;
 }
--- testing.php
+++ testing.php
@@ -8,7 +8,8 @@ $tag_name = 'approved';
 $tag_id = 'Pa9u';
 $email = 'louisl04@hotmail.com';
 $contact_id = 'SHO5QT';
-$campaign_name = 'zip';
+$campaign_name = 'zip_program_signups';
+$campaign_id = 'W6iSC';
 $affiliate_code = 'fake';
 $affiliate_link = 'https://wonkasoft.com';
 
@@ -18,7 +19,7 @@ $prep_data = array(
 		$tag_id,
 	),
 	'campaign_name'     => array(
-		'campaignId'    => $campaign_name,
+		'campaignId'    => $campaign_id,
 	),
 	'customFieldValues' => array(
 		'affiliate_code'    => $affiliate_code,
@@ -40,7 +41,7 @@ foreach ( $prep_data->tags as $value ) :
 	$tag_add = json_decode( json_encode( $tag_add ) );
 	array_push( $tags, $tag_add );
 endforeach;
-  $custom_fields = array();
+$custom_fields = array();
 foreach ( $prep_data->customFieldValues as $key => $value ) :
 	$custom_add = array(
 		'customFieldId' => $key,
@@ -50,51 +51,51 @@ foreach ( $prep_data->customFieldValues as $key => $value ) :
 	);
 	$custom_add = json_decode( json_encode( $custom_add ) );
 	array_push( $custom_fields, $custom_add );
-  endforeach;
+endforeach;
 
-  $payload  = array(
-	  'name'              => null,
-	  'campaign'          => $prep_data->campaign_name,
-	  'email'             => $prep_data->email,
-	  'dayOfCycle'        => null,
-	  'note'              => '',
-	  'scoring'           => null,
-	  'ipAddress'         => '',
-	  'tags'              => $tags,
-	  'customFieldValues' => $custom_fields,
-  );
+$payload  = array(
+	'name'              => null,
+	'campaign'          => $prep_data->campaign_name,
+	'email'             => $prep_data->email,
+	'dayOfCycle'        => null,
+	'note'              => '',
+	'scoring'           => null,
+	'ipAddress'         => '',
+	'tags'              => $tags,
+	'customFieldValues' => $custom_fields,
+);
 
-  $payload = json_encode( $payload );
-  echo "<pre>\n";
-	print_r( $payload );
+$payload = json_encode( $payload );
+echo "<pre>\n";
+print_r( $payload );
+echo "</pre>\n";
+
+$ch         = curl_init();
+$url        = $api_url . '/contacts/' . $contact_id;
+curl_setopt( $ch, CURLOPT_URL, $url );
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+curl_setopt( $ch, CURLOPT_HTTPHEADER, $headerdata );
+curl_setopt( $ch, CURLOPT_HEADER, false );
+curl_setopt( $ch, CURLOPT_POST, true );
+curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload );
+curl_setopt( $ch, CURLPROTO_HTTPS, true );
+
+$response = curl_exec( $ch );
+
+if ( false === $response ) :
+	curl_close( $ch );
+	$error_obj = array(
+		'error' => curl_error( $ch ),
+		'status'    => 'failed',
+	);
+	$error_obj = json_decode( $error_obj );
+	return $error_obj;
+else :
+	curl_close( $ch );
+	$response = json_decode( $response );
+
+	echo "<pre>\n";
+	print_r( $response );
 	echo "</pre>\n";
-
-  $ch         = curl_init();
-  $url        = $api_url . '/contacts/' . $contact_id;
-  curl_setopt( $ch, CURLOPT_URL, $url );
-  curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-  curl_setopt( $ch, CURLOPT_HTTPHEADER, $headerdata );
-  curl_setopt( $ch, CURLOPT_HEADER, false );
-  curl_setopt( $ch, CURLOPT_POST, true );
-  curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload );
-  curl_setopt( $ch, CURLPROTO_HTTPS, true );
-
-  $response = curl_exec( $ch );
-
-  if ( false === $response ) :
-	  curl_close( $ch );
-	  $error_obj = array(
-		  'error' => curl_error( $ch ),
-		  'status'    => 'failed',
-	  );
-	  $error_obj = json_decode( $error_obj );
-	  return $error_obj;
-  else :
-	  curl_close( $ch );
-	  $response = json_decode( $response );
-
-	  echo "<pre>\n";
-	  print_r( $response );
-	  echo "</pre>\n";
-	  return $response;
-  endif;
+	return $response;
+endif;
